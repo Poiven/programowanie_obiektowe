7e17c87e3f5fac53101e186954eaa9087ce7b58c
Poiven <147138126+Poiven@users.noreply.github.com>
2024-10-18 00:35:06 +0200

    Zestaw3

--- Zestaw3.java
+++ Zestaw3.java
@@ -3,14 +3,19 @@ public class Zestaw3 {
     public static void main(String[] args) {
 //        trojkiPitagorejskie(40);
 //        funKwaRozwiazania(20);
-//        funKwaDelta(20);
+//        funKwaDelta(5);
 //        liczbyPierwsze(20);
 //        liczbyPodzielne(4,5);
 //        System.out.println(silnia(0));
 //        piramida(5,1);
 //        System.out.println(silniaPodwojna(10));
 //        System.out.println(silniaM(1,10));
-        dwumianNewtona(14,2);
+//        dwumianNewtona(14,5);
+//        System.out.println(NWD(56,98));
+//        ciagFibonacciego(10);
+//        DoskonalaPrint(6);
+
+
     }
     //gotowe
     public static void trojkiPitagorejskie(int n){
@@ -37,16 +42,16 @@ public class Zestaw3 {
             }
         }
     }
-    // sqrt == i*i
+
     public static void funKwaDelta(int n){
         int delta = 0;
-        for (int i = 1; i <= n; i++){
-            for (int j = 0; j <= n; j++){
-                for (int k = 0; k <= n; k++){
-                    delta = j * j - (4 * i * k);
-                    if(delta >= 0){
-                        for (int a=1; a*a*a*a == delta; a++){
-                            System.out.println(i + " " + j + " " + k);
+        for (int i = -n; i <= n; i++){
+            if(i!=0){
+                for (int j = -n; j <= n; j++) {
+                    for (int k = -n; k <= n; k++) {
+                        delta = j * j - (4 * i * k);
+                        if (delta >= 0 && ((int) Math.sqrt(delta) * (int) Math.sqrt(delta) == delta)) {
+                            System.out.println(i + "," + j + "," + k);
                         }
                     }
                 }
@@ -83,8 +88,8 @@ public class Zestaw3 {
         }
     }
 //    gotowe
-    public static int silnia(int n){
-        int wynik=1;
+    public static long silnia(int n){
+        long wynik=1;
         for (int i = n;i > 0; i--){
             wynik*=i;
         }
@@ -123,10 +128,67 @@ public class Zestaw3 {
     }
 
     public static void dwumianNewtona(int n, int k){
-        double wynik = silnia(n)/(silnia(k)*silnia(n-k));
-        System.out.println(wynik);
+        if(n>k) {
+            long wynik = (silnia(n) / (silnia(k) * silnia(n - k)));
+            System.out.println(silnia(14));
+            System.out.println(silnia(2));
+            System.out.println(silnia(14-2));
+            System.out.println(silnia(14)/(2*silnia(12)));
+            System.out.println(wynik);
+        }
+    }
+    public static int NWD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    public static void ciagFibonacciego(int n)
+    {
+        for (int i = 0; i < n; i++) {
+            System.out.println(fibonacci(i));
+        }
+    }
+    public static boolean czyDoskonala(int n) {
+        if (n < 1) {
+            return false;
+        }
+        int suma = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                suma += i;
+            }
+        }
+        return suma == n;
+    }
+    public static void DoskonalaPrint(int n)
+    {
+        if (czyDoskonala(n))
+            System.out.println("Doskonala");
     }
 
+    public static boolean czyPalindrom(String wyraz) {
+        int length = wyraz.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (wyraz.charAt(i) != wyraz.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void wypiszPalindrom(String wyraz){
+        if (czyPalindrom(wyraz))
+            System.out.println(wyraz);
+    }
 
 
 
