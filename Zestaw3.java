0302a188fc775fb71cb10d45b6abfbccf6e8564f
Poiven <147138126+Poiven@users.noreply.github.com>
2024-11-08 15:40:48 +0100

    Zestaw3.java

--- Zestaw3.java
+++ Zestaw3.java
@@ -314,7 +314,23 @@ public class Zestaw3 {
     }
 
 
-
+/*
+    
+    public static void palindrom {
+            int number = 7337, reverse = 0;
+            while(number != 0)
+            {
+                int remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number = number/10;
+            }
+            System.out.println("Numer odwrocony " + reverse);
+            int liczba = 7337;
+                if(liczba - reverse == 0) System.out.println("jest palindromem");
+                else{
+                System.out.println("nie jest palindromem");
+                }
+            }*/
 
 
 
